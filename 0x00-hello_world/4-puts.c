a17d73544da6a677a65637ce6f5929e38d90797e
David Akpom <johjude16@gmail.com>
2023-10-05 09:49:22 -0700

    Hello, puts;
    ;

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This is the main to print
+ * Return: 0 when correct
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
