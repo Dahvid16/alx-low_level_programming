a38cc931b6ae0ffb65ba2cc345c9d440d569fb85
David Akpom <johjude16@gmail.com>
2023-10-05 09:56:37 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A c program that prints
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,");
+	return (0);
+}
